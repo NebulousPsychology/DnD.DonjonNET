94e9c914d0ad71b74c45bf8b32292fc38d30469e
NebulousPsychology <78229921+NebulousPsychology@users.noreply.github.com>
2025-07-25 10:56:49 -0700

    Original: doc comments and protected methods (#6)
    * original dungeongen methods to protected
    
    * original IDungeonRoom documented properties

--- Donjon/Original/IDungeonRoom.cs
+++ Donjon/Original/IDungeonRoom.cs
@@ -6,14 +6,45 @@ namespace Donjon.Original;
 public interface IDungeonRoom : IEquatable<IDungeonRoom>
 {
     public int id { get; }
+    /// <summary>
+    /// index of The North-most row enclosed by the room
+    /// </summary>
     public int north { get; }
+    /// <summary>
+    /// index of The South-most row enclosed by the room
+    /// </summary>
     public int south { get; }
+    /// <summary>
+    /// index of The East-most Col enclosed by the room
+    /// </summary>
     public int east { get; }
+    /// <summary>
+    /// index of The West-most Col enclosed by the room
+    /// </summary>
     public int west { get; }
+    /// <summary>
+    /// index of Starting Row enclosed
+    /// </summary>
     public int row { get; }
+    /// <summary>
+    /// index of starting col enclosed
+    /// </summary>
     public int col { get; }
+
+    /// <summary>
+    /// Count of rows.
+    /// <br/> A rectangle at (0,0) with Height=1, Width=1 covers only cell (0,0).
+    /// <br/> Height&lt;1 is meaningless as a room
+    /// </summary>
     public int height { get; }
+
+    /// <summary>
+    /// Count of columns.
+    /// <br/> A rectangle at (0,0) with Height=1, Width=1 covers only cell (0,0).
+    /// <br/> Width&lt;1 is meaningless as a room
+    /// </summary>
     public int width { get; }
+
     public int area => height * width;
     public int Perimeter => 2 * (height + width);
     public Dictionary<Cardinal, List<DoorData>> door { get; }
